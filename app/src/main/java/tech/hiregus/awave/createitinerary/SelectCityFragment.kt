a3f710d0ff86bab9880973e427b6ddd5def9daf6
Gustavo <gustavo@townsq.com.br>
2021-03-20 00:04:40 -0300

    Select city fragment and toolbar setup

--- app/src/main/java/tech/hiregus/awave/createitinerary/SelectCityFragment.kt
+++ app/src/main/java/tech/hiregus/awave/createitinerary/SelectCityFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import tech.hiregus.awave.R
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import tech.hiregus.awave.databinding.SelectCityFragmentBinding
 
 class SelectCityFragment : Fragment() {
+
+    private var _binding: SelectCityFragmentBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var viewModel: SelectCityViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.select_city_fragment, container, false)
+    ): View {
+        _binding = SelectCityFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
