72d258f2215ec4006e937b05e651ef62a9da5cc4
Gustavo <gustavo@townsq.com.br>
2021-03-26 17:26:19 -0300

    Renamed SelectCityFragment.kt to CreateItineraryFragment.kt

--- app/src/main/java/tech/hiregus/awave/createitinerary/CreateItineraryFragment.kt
+++ app/src/main/java/tech/hiregus/awave/createitinerary/CreateItineraryFragment.kt
@@ -6,22 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import tech.hiregus.awave.databinding.SelectCityFragmentBinding
+import tech.hiregus.awave.databinding.CreateItineraryFragmentBinding
 
-class SelectCityFragment : Fragment() {
+class CreateItineraryFragment : Fragment() {
 
-    private var _binding: SelectCityFragmentBinding? = null
+    private var _binding: CreateItineraryFragmentBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var viewModel: SelectCityViewModel
+    private lateinit var viewModel: CreateItineraryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = SelectCityFragmentBinding.inflate(inflater, container, false)
+        _binding = CreateItineraryFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
--- app/src/main/java/tech/hiregus/awave/createitinerary/CreateItineraryViewModel.kt
+++ app/src/main/java/tech/hiregus/awave/createitinerary/CreateItineraryViewModel.kt
@@ -2,6 +2,6 @@ package tech.hiregus.awave.createitinerary
 
 import androidx.lifecycle.ViewModel
 
-class SelectCityViewModel : ViewModel() {
+class CreateItineraryViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
